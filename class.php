27ddf8226839051ceff37e3b8453e04490dcdfb8
zupasigo <zupasigo@gmail.com>
2016-08-11 17:19:16 +0700

    Update class.php

--- class.php
+++ class.php
@@ -4,11 +4,13 @@ class Abcd
 {
     public function getMe()
     {
-        $tmp = "abc";
+        
         $sum = 0;
         for ($i = 0; $i < 10; $i++) {
             $sum = $sum + $i;
         }
+        
+        $tmp = "sum = $sum \n";
 
         return $sum;
     }
