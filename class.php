26573699f14747faf82a4e69d8e31ef195763241
zupasigo <zupasigo@gmail.com>
2016-08-11 17:16:04 +0700

    Update class.php

--- class.php
+++ class.php
@@ -1,14 +1,18 @@
 <?php
 
-class ABD
+class Abcd
 {
-    function getMe()
+    public function getMe()
     {
-      $sum = 0;
-        for ($i=0; $i<10; $i++) {
+        $sum = 0;
+        for ($i = 0; $i < 10; $i++) {
             $sum = $sum + $i;
         }
 
         return $sum;
     }
 }
+
+$abc = new Abcd;
+
+print $abc->getMe();
